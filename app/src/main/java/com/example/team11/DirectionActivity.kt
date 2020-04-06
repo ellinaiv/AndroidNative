bcec114d5306deb91725ab30d22012b9572568f1
marlenjarholt <pusikatt@live.no>
2020-04-06 09:32:26 +0200

    Lagt til at den viser veien fra deg til strendene

--- app/src/main/java/com/example/team11/DirectionActivity.kt
+++ app/src/main/java/com/example/team11/DirectionActivity.kt
@@ -2,6 +2,7 @@ package com.example.team11
 
 import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,11 +12,18 @@ import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import com.example.team11.viewmodels.DirectionActivityViewModel
-import com.example.team11.viewmodels.PlaceActivityViewModel
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.api.directions.v5.DirectionsCriteria
+import com.mapbox.api.directions.v5.MapboxDirections
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.core.constants.Constants
 import com.mapbox.geojson.FeatureCollection
+import com.mapbox.geojson.LineString
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
 import com.mapbox.mapboxsdk.location.LocationComponentOptions
@@ -24,15 +32,21 @@ import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.style.layers.LineLayer
+import com.mapbox.mapboxsdk.style.layers.Property
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DirectionActivity : AppCompatActivity() , PermissionsListener {
 
     private val viewModel: DirectionActivityViewModel by viewModels{ DirectionActivityViewModel.InstanceCreator() }
     private var permissionManager = PermissionsManager(this)
     private lateinit var mapboxMap: MapboxMap
+    private val ROUTE_SOURCE_ID = "ROUTE_SOURCE_ID"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,10 +72,30 @@ class DirectionActivity : AppCompatActivity() , PermissionsListener {
             this.mapboxMap = mapboxMap
             mapboxMap.setStyle(Style.MAPBOX_STREETS)
             mapboxMap.getStyle { style ->
+                style.addSource(GeoJsonSource(ROUTE_SOURCE_ID))
+                makeRouteLayer(style)
                 addDestinationMarker(place, style)
-                getRoute(place, style)
+                val position = CameraPosition.Builder()
+                    .target(LatLng(place.lat, place.lng))
+                    .zoom(10.0)
+                    .build()
+                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 2)
             }
         }
+    }
+
+    private fun makeRouteLayer(style: Style){
+        enableLocationComponent(style)
+        val routeLayer = LineLayer("ROUTE_LAYER_ID", ROUTE_SOURCE_ID)
+
+        routeLayer.setProperties(
+            PropertyFactory.lineCap(Property.LINE_CAP_ROUND),
+            PropertyFactory.lineJoin(Property.LINE_JOIN_ROUND),
+            PropertyFactory.lineWidth(5f),
+            PropertyFactory.lineColor(Color.parseColor("#009688"))
+        )
+
+        style.addLayer(routeLayer)
     }
 
     private fun addDestinationMarker(place: Place, style: Style){
@@ -87,12 +121,49 @@ class DirectionActivity : AppCompatActivity() , PermissionsListener {
         style.addLayer(symbolLayer)
     }
 
-    private fun getRoute(destination: Place, style: Style){
-        enableLocationComponent(style)
+    private fun getRoute(place: Place){
+        Log.d("tagDir", "er i getRoute")
+        val originLocation = mapboxMap.locationComponent.lastKnownLocation ?: return
+        val originPoint = Point.fromLngLat(originLocation.longitude, originLocation.latitude)
+
+        val client = MapboxDirections.builder()
+            .origin(originPoint)
+            .destination(Point.fromLngLat(place.lng, place.lat))
+            .overview(DirectionsCriteria.OVERVIEW_FULL)
+            .profile(DirectionsCriteria.PROFILE_CYCLING)
+            .accessToken(getString(R.string.access_token))
+            .steps(false)
+            .build()
+
+        client.enqueueCall(object : Callback<DirectionsResponse> {
+            override fun onResponse(call: Call<DirectionsResponse>, response: Response<DirectionsResponse>) {
+                if(response.body() == null || response.body()!!.routes().size < 1){
+                    Toast.makeText(this@DirectionActivity, "No routes found", Toast.LENGTH_SHORT).show()
+                    return
+                }
+
+                val currentRoute = response.body()!!.routes()[0]
+
+
+                mapboxMap.getStyle { style ->
+                    val source = style.getSourceAs<GeoJsonSource>(ROUTE_SOURCE_ID)
+                    source?.setGeoJson(
+                        LineString.fromPolyline(currentRoute.geometry()!!,
+                            Constants.PRECISION_6
+                        ))
+                }
+            }
+
+            override fun onFailure(call: Call<DirectionsResponse>, t: Throwable) {
+                Toast.makeText(this@DirectionActivity, "Error: " + t.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
     @SuppressLint("MissingPermission")
     private fun enableLocationComponent(style: Style){
+        Log.d("tagDir", "er i enableComponent")
         if(PermissionsManager.areLocationPermissionsGranted(this)){
             val customLocationComponentOptions =
                 LocationComponentOptions.builder(this)
@@ -111,6 +182,8 @@ class DirectionActivity : AppCompatActivity() , PermissionsListener {
                 cameraMode = CameraMode.TRACKING
                 renderMode = RenderMode.NORMAL
             }
+
+            getRoute(viewModel.place!!.value!!)
         }else{
             permissionManager = PermissionsManager(this)
             permissionManager.requestLocationPermissions(this)
@@ -118,7 +191,7 @@ class DirectionActivity : AppCompatActivity() , PermissionsListener {
     }
 
     override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
-        Toast.makeText(this, "Trengs for å vise deg veien til strand biaaatch", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, getString(R.string.viTrengerPosFordi), Toast.LENGTH_LONG).show()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
@@ -126,10 +199,11 @@ class DirectionActivity : AppCompatActivity() , PermissionsListener {
     }
 
     override fun onPermissionResult(granted: Boolean) {
+        Log.d("tagDir", "er i premission result")
         if(granted){
             enableLocationComponent(mapboxMap.style!!)
         }else{
-            Toast.makeText(this, "Kan ikke vise vei uten lokasjon", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, getString(R.string.ikkeViseVei), Toast.LENGTH_LONG).show()
             finish()
         }
     }
