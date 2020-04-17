45e315a3008b40cbaa918c866228b16225519a57
ellinaiv <ivlellina@gmail.com>
2020-04-17 11:32:04 +0200

    mvvm for PlacesListActivity

--- app/src/main/java/com/example/team11/PlacesListActivity.kt
+++ app/src/main/java/com/example/team11/PlacesListActivity.kt
@@ -2,8 +2,11 @@ package com.example.team11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.team11.viewmodels.PlacesListActivityViewModel
 import kotlinx.android.synthetic.main.activity_places_list.*
 
 
@@ -13,14 +16,18 @@ import kotlinx.android.synthetic.main.activity_places_list.*
  */
 class PlacesListActivity : AppCompatActivity() {
 
+    private val viewModel: PlacesListActivityViewModel by viewModels{ PlacesListActivityViewModel.InstanceCreator() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_places_list)
         val layoutManager = LinearLayoutManager(this)
 
-        val places = intent.getSerializableExtra("PLACES_LIST") as ArrayList<Place>
-        recycler_view.layoutManager = layoutManager as RecyclerView.LayoutManager?
-        recycler_view.adapter = ListAdapter(places, this)
+        viewModel.places!!.observe(this, Observer { places ->
+            recycler_view.layoutManager = layoutManager as RecyclerView.LayoutManager?
+            recycler_view.adapter = ListAdapter(places, this)
+
+        })
     }
 }
 
