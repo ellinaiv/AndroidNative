dabc7d4bf44040b1fa61d2442f4aaa005ba94969
ellinaiv <ivlellina@gmail.com>
2020-04-21 13:04:08 +0200

    fixed bottom margin for the map

--- app/src/main/java/com/example/team11/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/team11/ui/map/MapFragment.kt
@@ -8,9 +8,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
