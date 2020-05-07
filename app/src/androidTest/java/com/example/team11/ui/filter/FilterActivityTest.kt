3e755e72ebb6722d862184fe0b10383c24880955
marlenjarholt <pusikatt@live.no>
2020-05-07 13:06:36 +0200

    lagt til muligheten for å test seekbar

--- app/src/androidTest/java/com/example/team11/ui/filter/FilterActivityTest.kt
+++ app/src/androidTest/java/com/example/team11/ui/filter/FilterActivityTest.kt
@@ -1,12 +1,22 @@
 package com.example.team11.ui.filter
 
+import android.view.View
+import android.widget.SeekBar
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
-import org.junit.Assert.*
+import com.example.team11.R
+import org.hamcrest.Matcher
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4::class)
 class FilterActivityTest{
@@ -20,6 +30,28 @@ class FilterActivityTest{
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.team11", appContext.packageName)
+    }
+
+    @Test
+    fun testSeekBarWater(){
+        onView(withId(R.id.seekBarWater)).perform(setProgress(22))
+    }
+
+    private fun setProgress(progress: Int): ViewAction? {
+        return object : ViewAction {
+            override fun perform(uiController: UiController?, view: View) {
+                val seekBar = view as SeekBar
+                seekBar.progress = progress
+            }
+
+            override fun getDescription(): String {
+                return "Set a progress on a SeekBar"
+            }
+
+            override fun getConstraints(): Matcher<View> {
+                return ViewMatchers.isAssignableFrom(SeekBar::class.java)
+            }
+        }
     }
 
 }
