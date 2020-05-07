0e9575d30bbeb34196083de739e3e5e970ffa17e
marlenjarholt <pusikatt@live.no>
2020-05-07 13:25:31 +0200

    lagt til en seekbar test

--- app/src/androidTest/java/com/example/team11/ui/filter/FilterActivityTest.kt
+++ app/src/androidTest/java/com/example/team11/ui/filter/FilterActivityTest.kt
@@ -1,18 +1,22 @@
 package com.example.team11.ui.filter
 
+import android.content.Context
 import android.view.View
 import android.widget.SeekBar
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.example.team11.R
 import org.hamcrest.Matcher
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,19 +28,30 @@ class FilterActivityTest{
 
     @get :Rule
     val activityTestRule = ActivityTestRule(FilterActivity::class.java)
+    lateinit var appContext: Context
+
+    @Before
+    fun setup(){
+        appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    }
 
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.team11", appContext.packageName)
     }
 
     @Test
-    fun testSeekBarWater(){
-        onView(withId(R.id.seekBarWater)).perform(setProgress(22))
+    fun testSeekBarWater1(){
+        val temp = 22
+        onView(withId(R.id.seekBarWater)).perform(setProgress(temp))
+        onView(withId(R.id.textTempMidWater)).check(matches(withText(appContext.getString(R.string.tempC, temp))))
     }
 
+
+    //insperasjon til kode hentet fra: https://stackoverflow.com/questions/23659367/espresso-set-seekbar
+    //skrevet om til kotlin
+    //forfatter: Luigi Massa Gallerano
     private fun setProgress(progress: Int): ViewAction? {
         return object : ViewAction {
             override fun perform(uiController: UiController?, view: View) {
