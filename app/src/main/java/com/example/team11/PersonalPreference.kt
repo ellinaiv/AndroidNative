1069afd087433b5dcf36c4767a3a0ac93f3f3959
marlenjarholt <pusikatt@live.no>
2020-05-08 13:21:53 +0200

    flytta testene for pp

--- app/src/main/java/com/example/team11/PersonalPreference.kt
+++ app/src/main/java/com/example/team11/PersonalPreference.kt
@@ -1,7 +1,7 @@
 package com.example.team11
 
 
-data class PersonalPreference(
+ class PersonalPreference(
     val waterTempLow: Int = 0,
     var waterTempMid : Int = 15,
     val waterTempHigh: Int = 30,
@@ -13,4 +13,26 @@ data class PersonalPreference(
     var showAirCold: Boolean = true,
     var showAirWarm: Boolean = true,
     var showBasedOnWater: Boolean = true
-)
+){
+     /**
+      * Sjekker om et gitt sted har riktig kriterier mtp vanntempratur for å vises
+      * @param place stedet som skal sjekkes
+      * @return true hvis den oppfyller kriteriene false ellers
+      */
+     fun isTempWaterOk(place: Place): Boolean{
+         //TODO: Legge til tilfellet for vi ikke har noe data (da skal den bli true)
+         return ((showWaterWarm and (place.tempWater >= waterTempMid))
+                 or (showWaterCold and (place.tempWater < waterTempMid)))
+     }
+
+     /**
+      * Sjekker om et gitt sted har riktig kriterier mtp luftempratur for å vises
+      * @param place stedet som skal sjekkes
+      * @return true hvis den oppfyller kriteriene false ellers
+      */
+     fun isTempAirOk(place: Place): Boolean{
+         //TODO: Legge til tilfellet for vi ikke har noe data (da skal den bli true)
+         return ((showAirWarm and (place.tempAir >= airTempMid))
+                 or (showAirCold and (place.tempAir < airTempMid)))
+     }
+ }
