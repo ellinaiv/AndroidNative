2c2ff0c102eb40a6e3e1a751cec44fff4a832de8
ellinaiv <ivlellina@gmail.com>
2020-05-12 20:04:25 +0200

    fun testPlacesList

--- app/src/androidTest/java/com/example/team11/ui/placesList/PlacesListFragmentTest.kt
+++ app/src/androidTest/java/com/example/team11/ui/placesList/PlacesListFragmentTest.kt
@@ -1,28 +1,40 @@
 package com.example.team11.ui.placesList
 
+
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import com.example.team11.ui.bottomNavigation.MainActivity
-import org.junit.Assert.*
-import org.junit.Rule
 import org.junit.Test
-import com.example.team11.Place
 import com.example.team11.R
+import com.example.team11.ui.bottomNavigation.MainActivity
+import org.hamcrest.Matchers.allOf
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
 
-
+@RunWith(AndroidJUnit4::class)
 class PlacesListFragmentTes{
 
     @get:Rule
     val activityTestRule = ActivityTestRule(MainActivity::class.java)
 
+    @Before
+    fun setup(){
+        launchFragmentInContainer<PlacesListFragment>(themeResId = R.style.AppTheme)
+    }
+
     @Test
     fun testPlacesList(){
 
-        val place = Place(1,"Sørenga",1.1,2.2, false,22,25)
-        val data = arrayListOf<Place>()
-        data.add(place)
-        val scenario = launchFragmentInContainer<PlacesListFragment>(themeResId = R.style.AppTheme)
-
+        onView(withId(R.id.searchText))
+            .perform(typeText("Bekkensten"))
+        onView(withId(R.id.textName)).check(matches(withText("Bekkensten")))
     }
 
 }
