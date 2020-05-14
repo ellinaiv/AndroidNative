c074dcdb04f35bca1f7133443aec7d32c25c55db
Johanna Haarseth <johannph@uio.no>
2020-05-14 10:39:46 +0200

    Startet å fikse dao

--- app/src/main/java/com/example/team11/database/dao/WeatherForecastDao.kt
+++ app/src/main/java/com/example/team11/database/dao/WeatherForecastDao.kt
@@ -1,57 +1,31 @@
 package com.example.team11.database.dao
 
 import com.example.team11.database.entity.WeatherForecastDb
-
-package com.example.team11.database.dao
-
 import androidx.lifecycle.LiveData
 import androidx.room.*
-import com.example.team11.database.entity.Place
 
 @Dao
 interface WeatherForecastDao {
 
     /**
-     * Setter inn nye steder, men setter kun inn de stedene som ikke ligger der fra før, regner med at steder ikke endrer posisjon, og det er for at favorite-informasjonen skal bevares
-     * @param place en liste med alle stedene
+     * Setter inn nye forecats
+     * @param forecast, en liste med alle WeatherForecastDb
      */
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insertWeatherForecast(place: List<WeatherForecastDb>)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertWeatherForecast(forecast: List<WeatherForecastDb>)
 
     /**
-     * Legger til et sted som favoritt
-     * @param placeId iden til stedet
+     * Henter ut alle forecast for de neste timene
+     * @return liste med HourForecast
      */
-    @Query("UPDATE place  SET favorite = 1 WHERE id = :placeId")
-    fun addFavorite(placeId: Int)
-
-    /**
-     * Fjerner et sted som favoritt
-     * @param placeId iden til stedet
-     */
-    @Query("UPDATE place SET favorite = 0 WHERE id = :placeId")
-    fun removeFavorite(placeId: Int)
-
-    /**
-     * Sjekker om et sted er favoritt
-     * @param placeId iden til stedet
-     * @return Livedata<Boolean>
-     */
-    @Query("SELECT favorite FROM place WHERE id  = :placeId")
-    fun isPlaceFavorite(placeId: Int): LiveData<Boolean>
-
-    /**
-     * Henter ut alle steder
-     * @return liste med alle steder
-     */
-    @Query("SELECT * FROM place")
-    fun getPlaceList(): LiveData<List<Place>>
+    @Query("SELECT * FROM weather_forecast WHERE place_id = :placeId")
+    fun getHourForecast(placeId: Int): LiveData<List<WeatherForecastDb.HourForecast>>
 
     /**
      * Henter ut liste med alle steder som er favoritter
      * @return liste med steder som er favoritter
      */
     @Query("SELECT * FROM place WHERE favorite = 1")
-    fun getFavoritePlaceList(): LiveData<List<Place>>
+    fun getDayForecast(): LiveData<List<WeatherForecastDb.DayForecast>>
 
 }
--- app/src/main/java/com/example/team11/util/DbConstants.kt
+++ app/src/main/java/com/example/team11/util/DbConstants.kt
@@ -2,6 +2,6 @@ package com.example.team11.util
 
 object DbConstants {
     const val PLACE_TABLE_NAME = "place"
-    const val FORECAST_TABLE_NAME = "forecast"
+    const val FORECAST_TABLE_NAME = "weather_forecast"
     const val METADATA_TABLE_NAME = "metadata"
 }
