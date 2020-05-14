3b4c01b5b7fc1f608bea01d145c346a2df1b6da2
ellinaiv <ivlellina@gmail.com>
2020-05-14 15:43:43 +0200

    fun testFavouriteMarked

--- app/src/androidTest/java/com/example/team11/ui/favorites/FavoritesFragmentTest.kt
+++ app/src/androidTest/java/com/example/team11/ui/favorites/FavoritesFragmentTest.kt
@@ -1,10 +1,69 @@
 package com.example.team11.ui.favorites
 
-import org.junit.Assert.*
+import android.view.View
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.NoMatchingViewException
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import com.example.team11.R
+import com.example.team11.ui.bottomNavigation.MainActivity
+import com.example.team11.ui.placesList.PlacesListFragment
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.lang.Thread.sleep
+
 
 class FavoritesFragmentTest{
 
+    @get:Rule
+    val activityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun setUp(){
+        launchFragmentInContainer<PlacesListFragment>(themeResId = R.style.AppTheme)
+
+        onView(allOf(withId(R.id.recycler_viewPlaces), isDisplayed()))
+            .perform(click())
+        onView(withId(R.id.toggleFavourite))
+            .perform(click())
+    }
+
+    @Test
+    fun testFavouriteMarked(){
+        val activityTestRule = ActivityTestRule(MainActivity::class.java)
+
+        launchFragmentInContainer<PlacesListFragment>(themeResId = R.style.AppTheme)
+        onView(withId(R.id.recycler_viewPlaces))
+                .perform(ViewActions.swipeUp())
+        onView(allOf(withId(R.id.recycler_viewPlaces), isDisplayed()))
+            .perform(click())
+        onView(withId(R.id.toggleFavourite))
+            .perform(click())
+
+        launchFragmentInContainer<FavoritesFragment>(themeResId = R.style.AppTheme)
+        onView(withId(R.id.recycler_view)).check( RecyclerViewItemCountAssertion(2))
+        //onView(withId(R.id.textName)).check(matches(withText("Bekkensten")))
+    }
 
 
+}
+
+class RecyclerViewItemCountAssertion(private val expectedCount: Int) : ViewAssertion {
+    override fun check(view: View, noViewFoundException: NoMatchingViewException?) {
+        if (noViewFoundException != null) {
+            throw noViewFoundException
+        }
+        val recyclerView = view as RecyclerView
+        val adapter = recyclerView.adapter
+        assertThat(adapter!!.itemCount, `is`(expectedCount))
+    }
 
 }
